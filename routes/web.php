add19dfb8bcd612adb68bdb32f78809bbe285800
carlosefrain123 <carlosefrainchallo1@gmail.com>
2024-01-02 17:33:55 -0500

    02/01/2024 - 17:33 - Se ve todo el redireccionamiento de Laravel y como funciona

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+/* //TODO: Es simplificado
+Route::view('/', 'welcome');
 
-Route::get('/dashboard', function () {
+
+Y le podemos dar un nombre
+Route::view('/', 'welcome')->name('welcome');
+*/
+
+//TODO: Acá puedes crear los "/" que desees
+Route::get('/prueba', function () {
+    return "Welcome to our services page";
+});
+
+//TODO: Acá puedes crear los "/" que desees, colocando un parametro relacionado
+Route::get('/prueba/{prueba}', function ($prueba) {
+    return "Bienvenido ".$prueba;
+});
+
+//TODO: Acá podemos redireccionar
+Route::get('/prueba/{prueba?}', function ($prueba) {
+    if ($prueba==='2') {
+        return redirect('/prueba');
+    }
+    return "Bienvenido, mi estimado ".$prueba;
+});
+
+//TODO: Acá podemos redireccionar; pero con el nombre
+Route::get('/pruebaPapu', function () {
+    return "Bienvenido, este es una prueba de redirección";
+})->name('prueba.index');
+Route::get('/prueba/{prueba}', function ($prueba) {
+    if ($prueba==='2') {
+        return to_route('prueba.index');
+    }
+    return "Bienvenido, mi estimado ".$prueba;
+});
+/* Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -28,4 +63,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__.'/auth.php'; */

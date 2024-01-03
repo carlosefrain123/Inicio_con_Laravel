a03abdebc5dbbe3a2949c43b0b79317880bf8ca2
carlosefrain123 <carlosefrainchallo1@gmail.com>
2024-01-02 20:01:07 -0500

    02/01/2024 - 20:00 - Se agrupa las rutas, para que sea uno solo

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+
+Route::view('/', 'welcome');
 /* //TODO: Es simplificado
 Route::view('/', 'welcome');
 
@@ -26,24 +28,22 @@ Route::view('/', 'welcome')->name('welcome');
 */
 
 //TODO: Acá puedes crear los "/" que desees
-Route::get('/prueba', function () {
-    return "Welcome to our services page";
-});
 
-//TODO: Acá puedes crear los "/" que desees, colocando un parametro relacionado
+
+/* //TODO: Acá puedes crear los "/" que desees, colocando un parametro relacionado
 Route::get('/prueba/{prueba}', function ($prueba) {
     return "Bienvenido ".$prueba;
-});
+}); */
 
-//TODO: Acá podemos redireccionar
+/* //TODO: Acá podemos redireccionar
 Route::get('/prueba/{prueba?}', function ($prueba) {
     if ($prueba==='2') {
         return redirect('/prueba');
     }
     return "Bienvenido, mi estimado ".$prueba;
-});
+}); */
 
-//TODO: Acá podemos redireccionar; pero con el nombre
+/* //TODO: Acá podemos redireccionar; pero con el nombre
 Route::get('/pruebaPapu', function () {
     return "Bienvenido, este es una prueba de redirección";
 })->name('prueba.index');
@@ -52,15 +52,19 @@ Route::get('/prueba/{prueba}', function ($prueba) {
         return to_route('prueba.index');
     }
     return "Bienvenido, mi estimado ".$prueba;
-});
+}); */
 /* Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard'); */
 
 Route::middleware('auth')->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/prueba', function () {
+        return "Bienvenido a la prueba";
+    })->name('prueba.index');
 });
 
-require __DIR__.'/auth.php'; */
+require __DIR__ . '/auth.php';

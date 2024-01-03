2849c4b862e9c4f6bc03adc2a7d9f7635883b1b1
carlosefrain123 <carlosefrainchallo1@gmail.com>
2024-01-02 21:59:09 -0500

    02/01/2024 - 21:57 - Layout y navegación

--- routes/web.php
+++ routes/web.php
@@ -63,7 +63,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/prueba', function () {
-        return "Bienvenido a la prueba";
+        return view('prueba.index');
     })->name('prueba.index');
 });
 
